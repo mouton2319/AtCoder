7046f335c066573f9c815137634390d84d6293b6
mouton2319 <95887228+mouton2319@users.noreply.github.com>
2025-01-06 20:34:32 +0900

    Add files via upload

---
+++ AtCoder_Beginner_Contest_048/A-AtCoderContest/A/A/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+int main() {
+    std::string A;
+    std::string B;
+    std::string C;
+    std::cin >> A >> B >> C;
+
+    char A_0 = A[0];
+    char B_0 = B[0];
+    char C_0 = C[0];
+
+    std::cout << A_0 << B_0 << C_0 << std::endl;
+
+    return 0;
+
+}
